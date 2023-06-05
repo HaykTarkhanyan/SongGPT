0abb28ca34b014c2a275b8758983bc06e3ff26f6
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2023-06-05 22:15:04 +0400

    Added initial version no lyric song, no prompt enginnering

---
+++ app.py
@@ -0,0 +1,49 @@
+from random import choice
+import streamlit as st
+import logging 
+import openai
+import os 
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+st.write(os.environ)
+
+logging.basicConfig(filename='app.log', filemode='a', 
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
+                    level=logging.INFO)
+
+favorite_songs = ['Lake of fire by Nirvana', 'Aerials by SOAD']
+default_song = choice(favorite_songs)
+
+st.title("Your favorite songs about your topic")
+
+logging.info("App started")
+
+topic = st.text_input("Enter your topic here")
+song = st.multiselect("Select the song", ["Bohemian Rhapsody", "Real slim shady", "Դրախտի ալվան ծաղիկ"] + favorite_songs, 
+                      default=default_song)
+seriousness = st.slider("How serious is your topic?", 0, 2)
+
+seriousness_dict = {0: "serious", 1: "funny", 2: "very funny",}
+
+seriousness = seriousness_dict[seriousness]
+
+if st.button("Submit"):
+    logging.info(f"User submitted {topic} {song} and {seriousness}")
+    if not topic:
+        logging.warning("User did not enter a topic")
+        st.write("Please enter your topic")
+    else:    
+        prompt = f"Please change the text of the song `{song[0]}` to be about `{topic}`, make it {seriousness}, \
+                   keep the song structure and the rhymes. Provide your output in the following format \
+                   <Song name> \
+                   <Original lyrics> \
+                   <Changed lyrics>"
+        st.write(f"prompt: {prompt}")
+        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
+                                                       messages=[{"role": "user", "content": prompt}])
+        response = chat_completion.choices[0].message.content
+        st.write(response)
+        logging.info(f"Response: {response}")
+        st.balloons()
+    
