8977e961bbf183c45849bafa7d2373ac5b5ad35f
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2023-06-05 22:54:58 +0400

    added comments and response cleaning

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from random import choice
 import streamlit as st
 import logging 
 import openai
+from utils import clean_output
 import os 
 
-openai.api_key = os.environ["OPENAI_API_KEY"]
+os.environ["OPENAI_API_KEY"] = "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"
 
-st.write(os.environ)
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 logging.basicConfig(filename='app.log', filemode='a', 
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', 
@@ -20,6 +21,7 @@ st.title("Your favorite songs about your topic")
 logging.info("App started")
 
 topic = st.text_input("Enter your topic here")
+# TODO change the mutiselect to a dropdown
 song = st.multiselect("Select the song", ["Bohemian Rhapsody", "Real slim shady", "Դրախտի ալվան ծաղիկ"] + favorite_songs, 
                       default=default_song)
 seriousness = st.slider("How serious is your topic?", 0, 2)
@@ -39,11 +41,18 @@ if st.button("Submit"):
                    <Song name> \
                    <Original lyrics> \
                    <Changed lyrics>"
-        st.write(f"prompt: {prompt}")
+        st.write(f"prompt: {prompt}")\
+        
+        # TODO postprocess the output
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", 
                                                        messages=[{"role": "user", "content": prompt}])
         response = chat_completion.choices[0].message.content
+        
+        
         st.write(response)
+        st.subheader("Cleaned output")
+        st.write(clean_output(response))
+        
         logging.info(f"Response: {response}")
         st.balloons()
     
